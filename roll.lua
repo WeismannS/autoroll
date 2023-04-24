95d902a8e3cbbbae163c5854953d94ea65ffee70
Weismann <60429301+WeismannS@users.noreply.github.com>
2023-04-24 01:21:23 +0000

    Update roll.lua

--- roll.lua
+++ roll.lua
@@ -1,22 +1,26 @@
-
+function use(chosen)
     repeat wait() until game:IsLoaded()
-    pcall(function()
         repeat 
 wait()   
-until game:GetService("Players").LocalPlayer.PlayerGui.RerollClan.RollClanFrame.Clan:WaitForChild("Spins")
-spins = tonumber(game:GetService("Players").LocalPlayer.PlayerGui.RerollClan.RollClanFrame.Clan.Spins.Text:match("%d+%.?%d*"))
-       for i=0,spins do
-game:GetService("Players").LocalPlayer.PlayerGui.RerollClan.RollClanFrame.Clan.RRLastName.LocalScript.RR:InvokeServer("RRLastName")
-                    if tonumber(game:GetService("Players").LocalPlayer.PlayerGui.RerollClan.RollClanFrame.Clan.Spins.Text:match("%d+%.?%d*")) <=0 then
-local args = {
+until game:GetService("Players").LocalPlayer.PlayerGui.Gui.Ui.UiModule.Modules.Shop.RerollClan.RollClanFrame.Clan:WaitForChild("Spins")
+spins = tonumber(game:GetService("Players").LocalPlayer.PlayerGui.Gui.Ui.UiModule.Modules.Shop.RerollClan.RollClanFrame.Clan.Spins.Text:match("%d+%.?%d*"))
+       for i=0,10 do
+local ohString1 = "RRLastName"
+
+game:GetService("Players").LocalPlayer.PlayerGui.Gui.Ui.UiModule.Modules.Shop.RerollClan.RollClanFrame.Clan.RRLastName.LocalScript.RR:InvokeServer(ohString1)
+ wait(1)
+ print(game:GetService("Players").LocalPlayer.PlayerGui.Gui.Ui.UiModule.Modules.Shop.RerollClan.RollClanFrame.Clan.LName.Text)
+ if game:GetService("Players").LocalPlayer.PlayerGui.Gui.Ui.UiModule.Modules.Shop.RerollClan.RollClanFrame.Clan.LName == chosen then return end
+       end
+                       local args = {
     [1] = 1,
     [2] = "\255" 
 }
 game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("Gui"):WaitForChild("Ui"):WaitForChild("UiModule"):WaitForChild("Modules"):WaitForChild("Settings"):WaitForChild("Set"):InvokeServer(unpack(args))
- game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
+ game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)   
+       syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/WeismannS/autoroll/main/roll.lua'))()")
+
 end
-       end
-       syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/WeismannS/autoroll/main/roll'))()")
-       print("hi")
-    end)
-    
+ 
+    use("Kamado")
+   
